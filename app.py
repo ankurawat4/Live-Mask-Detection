03709bfc41233a26d2bdb6269ea788d1d0a79ee1
Ankur Rawat <44617027+ankurawat4@users.noreply.github.com>
2021-01-07 23:03:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 import cv2
 import numpy as np
 from PIL import Image
-import keras
+import tensorflow as tf
 
-model = keras.models.load_model('model.h5')
+model = tf.keras.models.load_model('model.h5')
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
 
 def about():
